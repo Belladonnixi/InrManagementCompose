19e6a4a845070b1a6427ad37207a48cadcca4c72
Jessica Ernst <jessica_ernst@icloud.com>
2022-07-08 12:30:13 +0200

    month

--- app/src/main/java/com/example/inr_management_md3/presentation/calendar/Month.kt
+++ app/src/main/java/com/example/inr_management_md3/presentation/calendar/Month.kt
@@ -1,2 +1,46 @@
+/**
+ * Copyright © 2022 Jessica Ernst
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries
+ * and frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.example.inr_management_md3.presentation.calendar
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.clearAndSetSemantics
+
+@Composable
+internal fun MonthHeader(
+    modifier: Modifier = Modifier,
+    month: String,
+    year: String
+) {
+    Row(modifier = modifier.clearAndSetSemantics { }) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = month,
+            style = MaterialTheme.typography.titleSmall
+        )
+        Text(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            text = year,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
+}
--- app/src/main/java/com/example/inr_management_md3/presentation/calendar/Week.kt
+++ app/src/main/java/com/example/inr_management_md3/presentation/calendar/Week.kt
@@ -1,5 +1,76 @@
+/**
+ * Copyright © 2022 Jessica Ernst
+ *
+ * This project and source code may use libraries or frameworks that are released under various
+ * Open-Source licenses. Use of those libraries and frameworks are governed by their own individual
+ * licenses.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.example.inr_management_md3.presentation.calendar
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.unit.dp
+import com.example.inr_management_md3.presentation.calendar.model.CalendarUiState
+import com.example.inr_management_md3.presentation.calendar.model.Week
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters
+
+@Composable
+internal fun DaysOfWeek(modifier: Modifier = Modifier) {
+    Row(modifier = modifier.clearAndSetSemantics { }) {
+        for (day in DayOfWeek.values()) {
+            DayOfWeekHeading(day = day.name.take(3))
+        }
+    }
+}
+
+@Composable
+internal fun Week(
+    calendarUiState: CalendarUiState,
+    week: Week,
+    onDayClicked: (LocalDate) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val beginningWeek = week.yearMonth.atDay(1).plusWeeks(week.number.toLong())
+    var currentDay = beginningWeek.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+
+    Box {
+        Row(modifier = modifier) {
+            Spacer(
+                Modifier
+                    .weight(1f)
+                    .heightIn(max = CELL_SIZE)
+            )
+            for (i in 0..6) {
+                if (currentDay.month == week.yearMonth.month) {
+                    Day(
+                        calendarState = calendarUiState,
+                        day = currentDay,
+                        onDayClicked = onDayClicked,
+                        month = week.yearMonth
+                    )
+                } else {
+                    Box(modifier = Modifier.size(CELL_SIZE))
+                }
+                currentDay = currentDay.plusDays(1)
+            }
+            Spacer(
+                Modifier
+                    .weight(1f)
+                    .heightIn(max = CELL_SIZE)
+            )
+        }
+    }
+}
 
 internal val CELL_SIZE = 48.dp
