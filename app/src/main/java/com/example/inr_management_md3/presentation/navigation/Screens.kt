2e2c144ae197c36c0581fa62812aa46eb919bc7e
Jessica Ernst <99251460+Belladonnixi@users.noreply.github.com>
2022-08-12 09:43:58 +0200

    cleaned up screens file

--- app/src/main/java/com/example/inr_management_md3/presentation/navigation/Screens.kt
+++ app/src/main/java/com/example/inr_management_md3/presentation/navigation/Screens.kt
@@ -30,38 +30,17 @@ sealed class Screens(
     object Dose : Screens("dose", R.string.dose, R.drawable.pill)
     object SetMeasure :
         Screens("setMeasure", R.string.set_measure, R.drawable.ic_launcher_foreground)
-
     object SetTargetRange :
         Screens("setTargetRange", R.string.set_target_range, R.drawable.ic_launcher_foreground)
-
     object SetMedicament :
         Screens("setMedicament", R.string.set_medicament, R.drawable.ic_launcher_foreground)
-
-    object SetDoseWeek : Screens("setDoseWeek", R.string.set_dose_week, R.drawable.pill)
-    object SetDoseInterval : Screens("setDoseInterval", R.string.set_dose_interval, R.drawable.pill)
-    object DoseAdjustment : Screens("doseAdjustment", R.string.dose_adjustment, R.drawable.pill)
     object InrMeasure : Screens("inrMeasure", R.string.measure, R.drawable.messure)
     object CalendarMonthView :
         Screens("calendarMonthView", R.string.calendar, R.drawable.ic_baseline_calendar_month_24)
-
-    object CalendarListView : Screens(
-        "calendarListView",
-        R.string.calendar_list_view,
-        R.drawable.ic_baseline_calendar_month_24
-    )
-
     object CalendarDay :
         Screens("calendarDay", R.string.calendar_day, R.drawable.ic_baseline_calendar_month_24)
-
-    object CalendarComment : Screens(
-        "calendarCommentScreen",
-        R.string.calendar_comment,
-        R.drawable.ic_baseline_calendar_month_24
-    )
-
     object StatisticCharts :
         Screens("statisticChartsScreen", R.string.statistc_charts, R.drawable.statistics)
-
     object About : Screens("about", R.string.about, R.drawable.ic_launcher_foreground)
 }
 
